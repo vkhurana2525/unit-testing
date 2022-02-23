a5fc1009a13c7c92e78f36cfdf453e5ffac026b0
Nitin K. Singh <nitin.rajput20@gmail.com>
2022-02-23 11:08:05 +0530

    Empty EmailService test file created.

---
+++ src/test/java/com/demo/service/EmailServiceTest.java
@@ -0,0 +1,9 @@
+package com.demo.service;
+
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EmailServiceTest {
+
+}
